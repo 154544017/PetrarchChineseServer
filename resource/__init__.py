67a76b7bda74e8c823c938ccf6344a87483630cc
154544017 <154544017@qq.com>
2019-12-12 01:52:33 +0800

    stupid

--- resource/__init__.py
+++ resource/__init__.py
@@ -12,18 +12,11 @@ def create_app():
 	db.init_app(app)
 	with app.app_context():
 		# Imports
-<<<<<<< HEAD
 		from .controller import userApi,helloApi,textLibApi,dictionaryApi
 		app.register_blueprint(helloApi.hellobp)
 		app.register_blueprint(blueprint=userApi.user_api, url_prefix='/user')
 		app.register_blueprint(blueprint=textLibApi.textLib_api, url_prefix='/textlibrary')
 		app.register_blueprint(blueprint=dictionaryApi.dictionary_api, url_prefix='/dic')
-=======
-		from .controller import userApi,helloApi,dictionaryApi
-		app.register_blueprint(helloApi.hellobp)
-		app.register_blueprint(blueprint=userApi.user_api, url_prefix='/user')
-		app.register_blueprint(blueprint=dictionaryApi.dictionary_api, url_prefix='/dic')
->>>>>>> 282b82a5fd7cadea4f0d6e37e060cb1c2920d90b
 		# Create tables for our models
 		db.create_all()
 		return app
